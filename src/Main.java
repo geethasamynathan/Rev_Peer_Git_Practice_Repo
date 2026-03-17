556c14bf08e321edfe142d8af650d54fa6e27eeb
Ananya Tayi <ananyatayi14@gmail.com>
2026-03-17 10:27:18 +0530

    student name

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
+        Scanner sc=new Scanner(System.in);
         System.out.println("Welcome to this Project");
+        System.out.println("enter the student name");
+        String name=sc.nextLine();
+        System.out.println("student name: "+name);
     }
 
 }
