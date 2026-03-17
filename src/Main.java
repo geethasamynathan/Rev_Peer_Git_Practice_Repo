6eeda726d3559774aa88051c952ca181f24c3c24
kancharlabrundarika13-hash <kancharlabrundarika13@gmail.com>
2026-03-17 11:09:19 +0530

    Input Of Student Name

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Welcome to this Project");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Student Name : ");
+        String StudentName = sc.nextLine();
+        System.out.println("Student Name : "+StudentName);
     }
 
 }
