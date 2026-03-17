3c66014215b3fd0196ced6130a1b587788a17790
gurleen-kaurr <gurleen.kaurrrrrr@gmail.com>
2026-03-17 10:52:58 +0530

    Updates main.java with a message

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Welcome to this Project");
+        System.out.println("This is Student Result System");
+
     }
 
 }
