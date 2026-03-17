cd2bcceccfd4c292c30b86703faef3b361480ba1
harishvreddy10 <harishvreddy10@gmail.com>
2026-03-17 10:47:44 +0530

    Student name

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args)
     {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name=scan.nextLine();
         System.out.println("Welcome to this Project");
+        System.out.println("Name: "+name);
     }
 
 }
