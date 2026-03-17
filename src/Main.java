360cf5101520f115b14f77273eb375c9c459cda4
GunnamDeekshitha <deekshithagunnam@gmail.com>
2026-03-17 10:22:18 +0530

    Feature110 added student details

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Welcome to this Project");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your registration number:");
+        String regId=sc.next();
+        System.out.println("Enter your name:");
+        String name=sc.next();
+        System.out.println("Enter your age:");
+        int age=sc.nextInt();
+        System.out.println("Student details are--");
+        System.out.println("Student:"+name);
+
     }
 
 }
