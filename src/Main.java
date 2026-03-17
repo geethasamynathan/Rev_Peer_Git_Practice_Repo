d0ccd7f7bdf05b71cdb805b3345231d4db1577ca
Tanu shree <tanushree.nik23@gmail.com>
2026-03-17 10:32:45 +0530

    added name input program

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
-public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Welcome to this Project");
-    }
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
+    }
 }
